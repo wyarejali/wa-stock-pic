258f048097cc1de2435a1646aa75a1bc62ab9494
Wyarej Ali <wyarejali@gmail.com>
2022-08-05 11:55:47 +0600

    Complete App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Container from './layouts/Container'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <Container>
     <App />
-  </Container>,
-  document.getElementById('root')
+  </Container>
 )
